24be240eb72748fc24c1b3eed64b4b5cd03cdb55
Martin Ouimet <mouimet@infini-soft.com>
2023-10-19 13:02:29 -0400

    openai

--- api/smartnotes.js
+++ api/smartnotes.js
@@ -1,37 +1,37 @@
-// import OpenAI from "openai";
+import OpenAI from "openai";
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-
-
-// module.exports = async (req, res) => {
-
-//   const chatCompletion = await openai.chat.completions.create({
-//     messages: [{ role: "user", content: "Say this is a test" }],
-//     model: "gpt-3.5-turbo",
-//   });
-
-//   res.status(200).send(chatCompletion.choices[0].message);
-// };
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 
-import {recognize} from 'tesseract.js';
 
 module.exports = async (req, res) => {
-    try {
-        if (!req.body.image) {
-            return res.status(400).send({ error: 'Image data is required' });
-        }
 
-        // The image can be sent as a base64 string or a URL
-        const image = req.body.image;
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
 
-        const result = await recognize(image, 'eng');
-        res.status(200).send({ text: result.data.text.trim() });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Failed to process image' });
-    }
+  res.status(200).send(chatCompletion.choices[0].message);
 };
+
+
+// import {recognize} from 'tesseract.js';
+
+// module.exports = async (req, res) => {
+//     try {
+//         if (!req.body.image) {
+//             return res.status(400).send({ error: 'Image data is required' });
+//         }
+
+//         // The image can be sent as a base64 string or a URL
+//         const image = req.body.image;
+
+//         const result = await recognize(image, 'eng');
+//         res.status(200).send({ text: result.data.text.trim() });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send({ error: 'Failed to process image' });
+//     }
+// };
