0cfba4d8f0b00adbcf5dd165192b0c722feffd95
Martin Ouimet <mouimet@infini-soft.com>
2023-10-19 12:56:20 -0400

    step wizard

--- src/apps/Demandes/StepFormWizard.jsx
+++ src/apps/Demandes/StepFormWizard.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
-import { Tabs, Tab, Button } from 'react-bootstrap';
+import { Tabs, Tab, Button, Card, ButtonGroup } from 'react-bootstrap';
 import { useParams, Link } from 'react-router-dom';
 import { getValue, useBrainStack } from '../../App';
 import Header from "../../layouts/Header";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import ContactForm from '../Contacts/ContactForm';
 import DemandesDetailsHeader from './DemandesDetailsHeader';
+import ServiceOptions from './ServiceOptions';
+import ContactOptions from './ContactOptions';
 
 function StepFormWizard() {
   const { id } = useParams()
   const [key, setKey] = useState('step1');
-    const bstack = useBrainStack()
+  const bstack = useBrainStack()
   const { list, search, create, update, delete: trash } = bstack.store.createCRUDObject('contacts')
   const [selectedId, setSelectedId] = useState(null)
   const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
@@ -29,35 +31,38 @@ function StepFormWizard() {
     <React.Fragment>
       <Header />
       <div className={"main main-file-manager p-3 p-lg-4"}>
-      <h1><Link to='/apps/demandes'><i class="ri-arrow-left-line"></i></Link> Demande</h1>
+        <h1><Link to='/apps/demandes'><i class="ri-arrow-left-line"></i></Link> Demande</h1>
         <DemandesDetailsHeader />
         <div>
           <Tabs
             id="step-form-wizard"
             activeKey={key}
             onSelect={(k) => setKey(k)}
+
           >
-            <Tab eventKey="step1" title="Contacts">
-              <ContactForm selectedId={selectedId} handleShowDeleteContact={()=>{setShowDeleteConfirm(true)}} handleUpdate={()=>{}}/>
+            <Tab eventKey="step1" title="Contacts" className="p-3">
+              <ContactOptions />
+              <ContactForm selectedId={selectedId} handleShowDeleteContact={() => { setShowDeleteConfirm(true) }} handleUpdate={() => { }} />
             </Tab>
-            <Tab eventKey="step2" title="Services">
-              {/* Your Step 2 form content goes here */}
-              <p>Content for Step 2</p>
+            <Tab eventKey="step2" title="Services" className="p-3">
+
+              <ServiceOptions />
+
             </Tab>
-            <Tab eventKey="step3" title="Disponibilités">
+            <Tab eventKey="step3" title="Disponibilités" className="p-3">
               {/* Your Step 3 form content goes here */}
               <p>Content for Step 3</p>
             </Tab>
           </Tabs>
 
-          <div className="mt-3">
-            <Button style={{marginRight:'0.5rem'}} onClick={handlePrev} disabled={key === 'step1'}>
+          {/* <div className="mt-3">
+            <Button style={{ marginRight: '0.5rem' }} onClick={handlePrev} disabled={key === 'step1'}>
               Previous
             </Button>
             <Button onClick={handleNext} className="ml-2" disabled={key === 'step3'}>
               Next
             </Button>
-          </div>
+          </div> */}
         </div>
       </div>
     </React.Fragment>
