b606d0baac7f3db9057a1c8b394e64fc94252992
Martin Ouimet <mouimet@infini-soft.com>
2023-10-19 09:40:53 -0400

    openai

--- api/smartnotes.js
+++ api/smartnotes.js
@@ -1,3 +1,17 @@
-module.exports = (req, res) => {
-  res.status(200).send('Hello from smart notes!');
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+
+module.exports = async (req, res) => {
+
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-3.5-turbo",
+  });
+
+  res.status(200).send(chatCompletion.choices[0].message);
 };
